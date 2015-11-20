b53430772e2463abf3dba3e8dad3062fd2451c1c
Rodrigo Costa <bacacosta@yahoo.com.br>
2015-11-21 00:55:30 +0100

    __update-bashrc and aliases.sh.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 # script-toolkit install script
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -7,13 +7,21 @@ SCRIPT_TOOLKIT_DIR="$HOME/.script-toolkit"
 __init() {
 	[[ -d $SCRIPT_TOOLKIT_DIR ]] && rm -r "$SCRIPT_TOOLKIT_DIR"
 	mkdir "$SCRIPT_TOOLKIT_DIR"
-	cp "$BASE_DIR"/src/* "$SCRIPT_TOOLKIT_DIR"
+	cp "$BASE_DIR/src"/* "$SCRIPT_TOOLKIT_DIR"
 }
 
 __generate-bootstrap() {
-	for SCRIPT in "$BASE_DIR"/src/*; do
-		echo ". \"$SCRIPT\"" >> "$SCRIPT_TOOLKIT_DIR"/bootstrap.sh
+	for SCRIPT in "$BASE_DIR/src"/*; do
+		echo ". \"$SCRIPT\"" >> "$SCRIPT_TOOLKIT_DIR/bootstrap.sh"
 	done
 }
 
-__init && __generate-bootstrap
+__update-bashrc() {
+	grep -q "script-toolkit" "$HOME/.bashrc" || cat <<- EOF >> "$HOME/.bashrc"
+
+			# script-toolkit
+			[[ -f $SCRIPT_TOOLKIT_DIR/bootstrap.sh ]] && . $SCRIPT_TOOLKIT_DIR/bootstrap.sh
+	EOF
+}
+
+__init && __generate-bootstrap && __update-bashrc
